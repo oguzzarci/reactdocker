ced9f8ff4e3f6762e88dd778b4abad06572e8ea4
oguz <oguzzarci@hotmail.com>
2020-04-21 22:49:03 +0300

    axios added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import logo from './k8s.png';
 import './App.css';
+import axios from 'axios'
+import React, { Component } from 'react'
+export default class App extends Component {
 
-function App() {
-  return (
+  state = {
+    data:''
+  }
+
+
+  componentDidMount(){
+
+    const url = "http://localhost:5000/api/getdata"
+
+    axios.get(url).then(response=> {
+      this.setState({
+        data:response.data.message
+      })
+    });
+
+  }
+
+
+
+  render() {
+      return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kubernetes React Web APP         
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Api Get Data : {this.state.data}        
+        </p>
       </header>
     </div>
-  );
+      )
+  }
 }
-
-export default App;
