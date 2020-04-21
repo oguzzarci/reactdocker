5ac20dfe4494d248e223e6426fa4d834930ff40f
oguz <oguzzarci@hotmail.com>
2020-04-21 22:59:50 +0300

    newlogo added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './k8s.png';
+import logotekno from './logotekno.png';
 import './App.css';
 import axios from 'axios'
 import React, { Component } from 'react'
@@ -11,7 +12,7 @@ export default class App extends Component {
 
   componentDidMount(){
 
-    const url = "http://localhost:5000/api/getdata"
+    const url = "http://192.168.1.23:5000/api/getdata"
 
     axios.get(url).then(response=> {
       this.setState({
@@ -27,6 +28,7 @@ export default class App extends Component {
       return (
     <div className="App">
       <header className="App-header">
+      <img src={logotekno} className="App-logo2" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Kubernetes React Web APP         
